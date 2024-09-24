5d29af8ddcc2908f5f48d7147c9715a878f85714
Rakib Islam <109210735+Koragg98@users.noreply.github.com>
2024-09-24 22:43:00 +0530

    Create SecondLargest.java

---
+++ Array Easy Problem/SecondLargest.java
@@ -0,0 +1,48 @@
+package ArrayEasyProblem;
+
+import java.util.Scanner;
+
+/**
+ * @author iraki
+ */
+
+public class SecondLargest {
+
+    public static int getSecondLargest(int[] arr) {
+        // Code Here
+        //Brute Force Approach
+        // Arrays.sort(arr);
+        // int largest=arr[arr.length-1];
+        // int secondLargest=-1;    //There may be a case where there is no secondLargest
+        // for(int i=arr.length-2;i>=0;i--)
+        // {
+        //     if(arr[i]>secondLargest && arr[i]!=largest)
+        //         secondLargest=arr[i];
+        // }
+        // return secondLargest;
+        
+        int largest=arr[0];
+        int secondLargest=-1;
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i]>largest)
+            {
+                secondLargest=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]!=largest && arr[i]>secondLargest)
+                secondLargest=arr[i];
+        }
+        return secondLargest;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        System.out.println(getSecondLargest(arr));
+    }
+
+}
