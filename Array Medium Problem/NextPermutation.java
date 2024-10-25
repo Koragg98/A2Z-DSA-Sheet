96adfff4f0c5de58d13c19cfa5dff75731eaa66f
Rakib Islam <109210735+Koragg98@users.noreply.github.com>
2024-10-25 21:37:44 +0530

    Create NextPermutation.java

---
+++ Array Medium Problem/NextPermutation.java
@@ -0,0 +1,53 @@
+package ArrayMediumProblem;
+import java.util.*;
+/**
+ * @author iraki
+ */
+public class NextPermutation {
+
+    public static void reverse(int arr[],int l,int h)
+    {
+        while(l<h)
+        {
+            int temp=arr[l];
+            arr[l]=arr[h];
+            arr[h]=temp;
+            l++;h--;
+        }
+    }
+    public static void nextPermutation(int[] nums) {
+        int n=nums.length;
+        int ind=-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                ind=i;
+                break;
+            }
+        }
+        if(ind==-1)
+        {
+            reverse(nums,0,n-1);
+        }
+        else
+        {
+            for(int i=n-1;i>ind;i--)
+            {
+                if(nums[i]>nums[ind])
+                {
+                    int temp=nums[i];
+                    nums[i]=nums[ind];
+                    nums[ind]=temp;
+                    break;
+                }
+            }
+            reverse(nums,ind+1,n-1);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,3,2};
+        nextPermutation(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
